165487907ed9fd120794774c12f6378b3bf4e2d3
Seth Thomas <sthomas@8thlight.com>
2022-05-16 13:56:15 -0400

    images pass spec

--- app/src/main/java/com/sethpthomas91/httpserver/interfaces/ServerLogicInterface.java
+++ app/src/main/java/com/sethpthomas91/httpserver/interfaces/ServerLogicInterface.java
@@ -5,6 +5,5 @@ import com.sethpthomas91.httpserver.response.HttpResponseWrapper;
 import java.io.IOException;
 
 public interface ServerLogicInterface {
-
     HttpResponseWrapper processRequest(HttpRequestInterface httpRequest) throws IOException;
 }
